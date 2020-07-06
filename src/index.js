cb3e1866db9b06e332ae5bb6178ee7044d7412e7
rfargo <renaldy_fargo@yahoo.com>
2020-07-06 23:54:42 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {BrowserRouter} from 'react-router-dom';
+import {BrowserRouter, HashRouter} from 'react-router-dom';
 
 
 ReactDOM.render(
-	<BrowserRouter  basename={process.env.PUBLIC_URL}>< App /></BrowserRouter >, document.getElementById("root")
+	<HashRouter  basename={process.env.PUBLIC_URL}>< App /></HashRouter >, document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
