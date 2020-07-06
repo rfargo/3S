97d4c6b31ec3cd1055de2bde55dfe48768cc8e25
rfargo <renaldy_fargo@yahoo.com>
2020-07-06 23:25:26 +0800

    Updates

---
+++ precache-manifest.e78e2fb91e523b8fdb41d0b93913fb23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "423b3f1ee7c01ef413beb73aa0b31105",
+    "url": "/3s/index.html"
+  },
+  {
+    "revision": "edbc347751816c761df3",
+    "url": "/3s/static/css/main.daa4ec40.chunk.css"
+  },
+  {
+    "revision": "14857c6597f8883362d4",
+    "url": "/3s/static/js/2.4dd78ced.chunk.js"
+  },
+  {
+    "revision": "edbc347751816c761df3",
+    "url": "/3s/static/js/main.6cb3ab8a.chunk.js"
+  },
+  {
+    "revision": "3a45a48f202f2cb3c0a5",
+    "url": "/3s/static/js/runtime-main.4bcb8529.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/3s/static/media/logo.25bf045c.svg"
+  }
+]);
